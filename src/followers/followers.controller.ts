6561a58228372c291bffc532b961af657a3dd9e9
franco22bellido <franco.bellido22@gmail.com>
2024-04-17 01:08:58 -0300

    authGuard added

--- src/followers/followers.controller.ts
+++ src/followers/followers.controller.ts
@@ -1,25 +1,29 @@
-import { Controller, Get, Param, ParseIntPipe, Post } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
 import { FollowersService } from './followers.service';
+import { AuthGuard } from '../auth/auth.guard';
 
 @Controller('followers')
+@UseGuards(AuthGuard)
 export class FollowersController {
+  constructor(private readonly _followersService: FollowersService) {}
 
-    
-
-    constructor(private readonly _followersService : FollowersService){}
-
-
-    @Get('/followers/:userId')
-    async getFollowers (@Param('userId', ParseIntPipe) userId: number){
-        return this._followersService.getFollowers(userId);
-    }
-    @Get('/idols/:userId')
-    async getIdols(@Param('userId', ParseIntPipe) userId: number){
-        return await this._followersService.getIdols(userId);
-    }
-    @Post('/follow-one/:userToFollow')
-    async followOne(@Param('userToFollow', ParseIntPipe) userToFollow: number){
-        return await this._followersService.followUser(1 ,userToFollow);
-    }
-    
+  @Get('/followers/:userId')
+  async getFollowers(@Param('userId', ParseIntPipe) userId: number) {
+    return this._followersService.getFollowers(userId);
+  }
+  @Get('/idols/:userId')
+  async getIdols(@Param('userId', ParseIntPipe) userId: number) {
+    return await this._followersService.getIdols(userId);
+  }
+  @Post('/follow-one/:userToFollow')
+  async followOne(@Param('userToFollow', ParseIntPipe) userToFollow: number) {
+    return await this._followersService.followUser(1, userToFollow);
+  }
 }
