238354c640fc0fba83c6e34bfcb1ca56ee728322
Franco Bellido <118084537+franco22bellido@users.noreply.github.com>
2024-05-25 15:28:03 -0300

    Update main.ts
    Origin true: cors

--- src/main.ts
+++ src/main.ts
@@ -7,8 +7,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: [process.env.FRONT_URL],
-    credentials: true,
+        origin: true,
+        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+        credentials: true,
+    
   });
   app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe());
