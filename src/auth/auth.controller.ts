338fc98b3e79e25341a054a7ec0bddc165bc05b7
franco22bellido <franco.bellido22@gmail.com>
2024-05-26 18:27:46 -0300

    add the same config in logout as login

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -54,7 +54,15 @@ export class AuthController {
 
   @Delete('/logout')
   logout(@Res() response: Response) {
-    response.clearCookie(this._configService.get<string>(KeysEnum.COOKIE_NAME));
+    response.clearCookie(
+      this._configService.get<string>(KeysEnum.COOKIE_NAME),
+      {
+        sameSite: 'none',
+        secure: true,
+        httpOnly: true,
+        maxAge: 1000 * 60 * 60 * 24 * 7,
+      },
+    );
     return response.status(204);
   }
 }
