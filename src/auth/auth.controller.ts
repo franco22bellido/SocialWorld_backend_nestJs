8da20a33c92520fd52e4423a7163c25db12d16b7
franco22bellido <franco.bellido22@gmail.com>
2024-05-26 18:21:25 -0300

    me falto el return

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -51,10 +51,10 @@ export class AuthController {
   getProfile(@Req() requestUser: RequestUser) {
     return { message: 'it is your profile!', user: requestUser.user };
   }
-  @UseGuards(AuthGuard)
+
   @Delete('/logout')
   logout(@Res() response: Response) {
     response.clearCookie(this._configService.get<string>(KeysEnum.COOKIE_NAME));
-    response.status(204);
+    return response.status(204);
   }
 }
